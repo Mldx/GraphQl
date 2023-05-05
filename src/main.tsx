f963b2f2280968133bb7302ada78f8968e76b217
mldx <anton.zaprutski@gmail.com>
2023-05-05 20:44:32 +0300

    feat: #1 configure develop branch

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
+
+import 'normalize.css';
+import './index.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
