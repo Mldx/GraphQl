57d6369f9b607a6efa0b2b4d053c1804946fe741
mldx <anton.zaprutski@gmail.com>
2023-05-09 07:36:15 +0300

    fix: #2 fix auth initialize

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,4 +9,5 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-export const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+getAuth(firebaseApp);
