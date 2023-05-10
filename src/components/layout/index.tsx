eb31a3d8ad69a6f5a696d8ff418311a4a6bd059b
mldx <anton.zaprutski@gmail.com>
2023-05-11 01:29:03 +0300

    refactor: change directory structural Footer component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './../header';
 import style from './style.module.scss';
-import { Footer } from './../footer';
+import { Footer } from '../Footer';
 
 export const Layout: React.FC = () => (
   <div className={style.layoutWrap}>
