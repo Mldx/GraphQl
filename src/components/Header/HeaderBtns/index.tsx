9027db440183e0aa0896c0f6e281cbfe72f82e82
mldx <anton.zaprutski@gmail.com>
2023-05-11 07:08:55 +0300

    feat: add check auth to header

--- src/components/Header/HeaderBtns/index.tsx
+++ src/components/Header/HeaderBtns/index.tsx
@@ -1,5 +1,21 @@
 import style from './HeaderBtns.module.scss';
+import NavigateButton from '../../NavigateButton';
+import LogoutButton from '../../LogoutButton';
 
-export const HeaderBtns: React.FC = () => {
-  return <div className={style.btnsWrapper}></div>;
+export const HeaderBtns: React.FC<{ isLogin: boolean }> = ({ isLogin }) => {
+  return (
+    <div className={style.btnsWrapper}>
+      {isLogin ? (
+        <>
+          <NavigateButton text="go to main page" url="/main" />
+          <LogoutButton />
+        </>
+      ) : (
+        <>
+          <NavigateButton text="signin" url="/login" />
+          <NavigateButton text="signup" url="/signup" />
+        </>
+      )}
+    </div>
+  );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,29 @@
 import style from './Header.module.scss';
 import { Navbar } from './Navbar';
 import { HeaderLogo } from './HeaderLogo';
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { HeaderBtns } from './HeaderBtns';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 
 export const Header: React.FC = () => {
   const sticky = useStickyHeader(1);
+
+  const [currentUser, setCurrentUser] = useState<null | User>(null);
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      user ? setCurrentUser(user) : setCurrentUser(null);
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <header className={`${sticky ? `${style.sticky}` : `${style.header}`}`}>
       <div className={style.wrapper}>
         <HeaderLogo />
         <Navbar />
-        <HeaderBtns />
+        <HeaderBtns isLogin={!!currentUser} />
       </div>
     </header>
   );
