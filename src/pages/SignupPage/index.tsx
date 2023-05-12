e4f65f91e9f7ea33716f498693b49455f2cd9e40
mldx <anton.zaprutski@gmail.com>
2023-05-12 06:15:39 +0300

    feat: add some styles to link

--- src/pages/SignupPage/index.tsx
+++ src/pages/SignupPage/index.tsx
@@ -28,7 +28,10 @@ function SignupPage() {
     <div className={styles.signup_page_wrapper}>
       <RegisterForm />
       <div>
-        Already have an account? <Link to="/login">login</Link>
+        Already have an account?&nbsp;
+        <Link to="/login" className={styles.link}>
+          login
+        </Link>
       </div>
     </div>
   );
