65022569a1f3e76a6cd8edb35480fbe870a33396
mldx <anton.zaprutski@gmail.com>
2023-05-17 15:22:23 +0300

    feat: save language to localStorage

--- src/components/Header/SwitchLng/index.tsx
+++ src/components/Header/SwitchLng/index.tsx
@@ -1,16 +1,23 @@
 import styles from './SwitchLng.module.scss';
 import { changeLanguage } from 'i18next';
+import { useState } from 'react';
 
 function SwitchLng() {
+  const [lng, setLng] = useState(localStorage.getItem('i18nextLng'));
   return (
     <div className={styles.switchLng}>
       <label className={styles.label} onClick={() => changeLanguage('en')}>
-        <input name="switchLng" type="radio" defaultChecked />
+        <input
+          name="switchLng"
+          type="radio"
+          onChange={() => setLng('en')}
+          checked={lng === ('en' || null)}
+        />
         <span>EN</span>
       </label>
       /
       <label className={styles.label} onClick={() => changeLanguage('ru')}>
-        <input name="switchLng" type="radio" />
+        <input name="switchLng" type="radio" onChange={() => setLng('ru')} checked={lng === 'ru'} />
         <span>RU</span>
       </label>
     </div>
