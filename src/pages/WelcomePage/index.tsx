f139f463c5cd8bb5483d8280710f0c0f92011afc
mldx <anton.zaprutski@gmail.com>
2023-05-18 15:08:40 +0300

    fix: fix conflict

--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -11,11 +11,7 @@ export const WelcomePage: React.FC = () => {
       <div className={style.welcome_page_wrap}>
         <div className={style.title_wrap}>
           <h1 className={style.title_main}>GraphiQL</h1>
-          <p className={style.title}>
-            the GraphQL integrated development environment (IDE). It’s a powerful (and all-around
-            awesome) tool you’ll use often while building Gatsby websites. It offers syntax
-            highlighting, intellisense autocompletion, automatic documentation, and much more.
-          </p>
+          <p className={style.title}> {t('welcomePage.description')}</p>
         </div>
         <div className={style.dev_container}>
           {developers.map((el: IDeveloper) => (
