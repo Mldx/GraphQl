7d1059fc9d61a9d7871d385ca0e9bf486209f472
Yuliya Aleinik <aleinikya@mail.ru>
2023-05-19 17:14:41 +0300

    feat: create schema

--- src/pages/Schema/index.tsx
+++ src/pages/Schema/index.tsx
@@ -6,39 +6,8 @@ import { schemaFromExecutor } from '@graphql-tools/wrap';
 import { GraphQLSchema } from 'graphql/type';
 
 export const Schema = () => {
-  //11111
-  {/* <GraphiQL
-    fetcher={async (graphQLParams: any) => {
-      const data = await fetch('https://rickandmortyapi.com/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-
-        body: JSON.stringify(graphQLParams),
-        credentials: 'same-origin',
-      });
-      const x = data.json().catch(() => data.text());
-      console.log(x);
-      return x;
-    }}
-  />; */}
   const [myGraphQLSchema, setMyGraphQLSchema] = useState<GraphQLSchema>();
   useEffect(() => {
-    //22222
-    /* const makeRequest = async () => {
-      return fetch('https://rickandmortyapi.com/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-      })
-        .then((res) => res.json())
-        .then((result) => console.log(result));
-    };
-    makeRequest(); */
-    //33333
     const fetchSchema = async () => {
       const remoteExecutor = buildHTTPExecutor({ endpoint: 'https://rickandmortyapi.com/graphql' });
 
@@ -47,10 +16,11 @@ export const Schema = () => {
         executor: remoteExecutor,
       };
       const fields = postsSubschema.schema.getQueryType()?.getFields();
-      const result = JSON.parse(JSON.stringify(fields));
+      //const result = JSON.parse(JSON.stringify(fields));
       setMyGraphQLSchema(postsSubschema.schema);
-      console.log(myGraphQLSchema);
     };
     fetchSchema();
   }, []);
+  console.log(myGraphQLSchema);
+  return <div> </div>;
 };
