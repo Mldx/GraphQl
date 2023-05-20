ce602882913a8e0c95a4ede03273279138efae27
mldx <anton.zaprutski@gmail.com>
2023-05-20 22:37:36 +0300

    feat: test

--- src/components/Sch/index.tsx
+++ src/components/Sch/index.tsx
@@ -1,8 +1,17 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  GraphQLSchema,
+  buildClientSchema,
+  GraphQLObjectType,
+  GraphQLNamedType,
+  GraphQLInputObjectType,
+  GraphQLScalarType,
+} from 'graphql';
+import { Link } from 'react-router-dom';
+import styles from './Sch.module.scss';
 
 const serverUrl = 'https://rickandmortyapi.com/graphql';
 const sdlQuery = `
-   
     query IntrospectionQuery {
       __schema {
         
@@ -109,45 +118,126 @@ const options = {
   body: JSON.stringify({ query: sdlQuery }),
 };
 
-// Отправляем запрос с помощью fetch
-
 function Sch() {
-  const [data, setData] = useState<object | null>(null);
   const [error, setError] = useState(null);
-  const [currentLink, setCurrentLink] = useState('Characters');
+  const [schema, setSchema] = useState<GraphQLSchema | null>(null);
+  const [curType, setCurType] = useState<GraphQLObjectType | GraphQLNamedType | null | undefined>(
+    null
+  );
 
   useEffect(() => {
     fetch(serverUrl, options)
       .then((response) => response.json())
       .then((data) => {
-        const schemaSDL = JSON.stringify(data, null, 2);
-        const dataObj = JSON.parse(schemaSDL);
-        console.log(dataObj.data.__schema);
-        setData(dataObj.data.__schema);
+        const schema = buildClientSchema(data.data);
+        setSchema(schema);
+        setCurType(schema.getQueryType());
       })
       .catch((error) => {
         setError(error.message);
       });
   }, []);
 
-  return (
-    <>
-      <div>{error}</div>
-      {data && (
-        <>
-          <div>{`type: ${currentLink}`}</div>
-          <br />
-          <div>
-            {data.types
-              .find((value: object) => value.name.toLowerCase() === currentLink.toLowerCase())
-              .fields?.map((v: string) => (
-                <p key={v.name}>{v.name}</p>
-              ))}
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!schema) {
+    return <div>Loading...</div>;
+  }
+  const handleClickType = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const text = e.currentTarget.text.replace(/[^a-zA-Z]/g, '');
+    console.log(text);
+    setCurType(schema.getType(text));
+    console.log(schema.getType(text));
+  };
+  const handleClickField = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const text = e.currentTarget.text;
+    console.log(text);
+    if (curType instanceof GraphQLObjectType || curType instanceof GraphQLInputObjectType) {
+      console.log(curType.getFields()[text]);
+    }
+  };
+
+  if (curType instanceof GraphQLObjectType) {
+    const fields = curType ? Object.values(curType.getFields()) : [];
+    return (
+      <div className={styles.main_container}>
+        <div>{curType?.name}</div>
+        {fields.map((field, index) => (
+          <div key={index}>
+            <div>
+              <Link to="" onClick={(e) => handleClickField(e)}>
+                {field.name}
+              </Link>
+              {!!field.args.length && (
+                <>
+                  (
+                  <span>
+                    {field.args.map((arg, argIndex) => (
+                      <div key={argIndex} className={styles.arg}>
+                        <span>
+                          <span>{arg.name}</span>:
+                          <Link to="" onClick={(e) => handleClickType(e)}>
+                            {arg.type.toString()}
+                          </Link>
+                        </span>
+                      </div>
+                    ))}
+                  </span>
+                  )
+                </>
+              )}
+              :
+              <Link to="" onClick={(e) => handleClickType(e)}>
+                {` ${field.type.toString()}`}
+              </Link>
+            </div>
+            <div className={styles.desc}>
+              <p>{field.description}</p>
+            </div>
           </div>
-        </>
-      )}
-    </>
-  );
+        ))}
+      </div>
+    );
+  }
+
+  if (curType instanceof GraphQLInputObjectType) {
+    const fields = curType ? Object.values(curType.getFields()) : [];
+    return (
+      <div className={styles.main_container}>
+        <div>{curType?.name}</div>
+        {fields.map((field, index) => (
+          <div key={index}>
+            <div>
+              <Link to="" onClick={(e) => handleClickField(e)}>
+                {field.name}
+              </Link>
+              :
+              <Link to="" onClick={(e) => handleClickType(e)}>
+                {` ${field.type.toString()}`}
+              </Link>
+            </div>
+            <div className={styles.desc}>
+              <p>{field.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (curType instanceof GraphQLScalarType) {
+    return (
+      <div className={styles.main_container}>
+        <div>{curType?.name}</div>
+        <div className={styles.desc}>
+          <p>{curType.description && curType.description.replaceAll('`', '')}</p>
+        </div>
+      </div>
+    );
+  }
+  return <></>;
 }
 
 export default Sch;
