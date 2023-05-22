9abcb076611c2fe46cb23d90c0c894ae3a95f332
mldx <anton.zaprutski@gmail.com>
2023-05-22 21:51:14 +0300

    feat: change IntrospectionQuery

--- src/components/Sch/index.tsx
+++ src/components/Sch/index.tsx
@@ -11,113 +11,15 @@ import {
 } from 'graphql';
 import { Link } from 'react-router-dom';
 import styles from './Sch.module.scss';
+import { getIntrospectionQuery } from 'graphql/index';
 
 const serverUrl = 'https://rickandmortyapi.com/graphql';
-const sdlQuery = `
-    query IntrospectionQuery {
-      __schema {
-        
-        queryType { name }
-        mutationType { name }
-        subscriptionType { name }
-        types {
-          ...FullType
-        }
-        directives {
-          name
-          description
-          
-          locations
-          args {
-            ...InputValue
-          }
-        }
-      }
-    }
-
-    fragment FullType on __Type {
-      kind
-      name
-      description
-      
-      fields(includeDeprecated: true) {
-        name
-        description
-        args {
-          ...InputValue
-        }
-        type {
-          ...TypeRef
-        }
-        isDeprecated
-        deprecationReason
-      }
-      inputFields {
-        ...InputValue
-      }
-      interfaces {
-        ...TypeRef
-      }
-      enumValues(includeDeprecated: true) {
-        name
-        description
-        isDeprecated
-        deprecationReason
-      }
-      possibleTypes {
-        ...TypeRef
-      }
-    }
-
-    fragment InputValue on __InputValue {
-      name
-      description
-      type { ...TypeRef }
-      defaultValue
-      
-      
-    }
-
-    fragment TypeRef on __Type {
-      kind
-      name
-      ofType {
-        kind
-        name
-        ofType {
-          kind
-          name
-          ofType {
-            kind
-            name
-            ofType {
-              kind
-              name
-              ofType {
-                kind
-                name
-                ofType {
-                  kind
-                  name
-                  ofType {
-                    kind
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  
-`;
 const options = {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
-  body: JSON.stringify({ query: sdlQuery }),
+  body: JSON.stringify({ query: getIntrospectionQuery() }),
 };
 
 function Sch() {
