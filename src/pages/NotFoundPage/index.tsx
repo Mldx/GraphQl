fef1611f3b4d505b2b384586077d08ac4496c4fd
Yuliya Aleinik <aleinikya@mail.ru>
2023-05-27 20:19:08 +0300

    fix: delete imgs

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -3,7 +3,7 @@ import style from './NotFoundPage.module.scss';
 export const NotFoundPage: React.FC = () => {
   return (
     <div className={style.wrapper}>
-      <img className={style.img} src="/public/404.png" alt="" />
+      <img className={style.img} src="/404.png" alt="" />
     </div>
   );
 };
