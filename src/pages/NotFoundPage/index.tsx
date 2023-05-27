d263beb55f37b86c9943bf2645d969a960048a39
Yuliya Aleinik <aleinikya@mail.ru>
2023-05-27 16:28:13 +0300

    fix: fix sticky, design

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,9 @@
 import style from './NotFoundPage.module.scss';
 
 export const NotFoundPage: React.FC = () => {
-  return <div className={style.wrapper}></div>;
+  return (
+    <div className={style.wrapper}>
+      <img className={style.img} src="/public/404.png" alt="" />
+    </div>
+  );
 };
