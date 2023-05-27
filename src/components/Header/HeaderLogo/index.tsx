72cc6a209ccf646b149a41845fbf4f04574dd8ac
mldx <anton.zaprutski@gmail.com>
2023-05-28 01:13:42 +0300

    feat: some change and fix styles

--- src/components/Header/HeaderLogo/index.tsx
+++ src/components/Header/HeaderLogo/index.tsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 export const HeaderLogo: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div className={style.wrapper} onClick={() => navigate('/')}>
-      <img className={style.logo_img} src={logoImg} alt="graphql" />
+    <div draggable={false} className={style.wrapper} onClick={() => navigate('/')}>
+      <img draggable={false} className={style.logo_img} src={logoImg} alt="graphql" />
       <p className={style.logo}>Graphiql</p>
     </div>
   );
